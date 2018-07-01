219d665b5af9bc846db20b543726ef8ef04eef11
reritom <reikudjinn@gmail.com>
2018-07-01 16:30:24 +0200

    Connection locking and unlocking updated

--- esvi/query_executor.py
+++ esvi/query_executor.py
@@ -11,38 +11,11 @@ class QueryExecutor():
         print("Executor connection is {}".format(self.cnx))
 
     def execute(self, query):
-        print("Executing query")
-
-    def _lock(self):
-        lock_path = os.path.join(self.this_dir, '.esvi.lock')
-        if os.path.isfile(lock_path):
-            with open(lock_path, 'r') as f:
-                lock_file = f.read()
-
-            try:
-                lock_file = json.loads(lock_file)
-            except:
-                print("Failed to read lock json")
-            return
-
-            # Here we will check if there has been a timeout
-
-        self.session_id = str(uuid.uuid4())
-
-        lock_bom = {'locked_at': datetime.datetime.now().isoformat(),
-                    'locked_by': self.session_id,
-                    'db_identifier': self.db_path}
-
-        with open(lock_path, 'w') as f:
-            f.write(json.dumps(lock_bom))
-
-    def _unlock(self):
-        lock_path = os.path.join(self.this_dir, '.esvi.lock')
-        if os.path.isfile(lock_path):
-            with open(lock_path, 'r') as f:
-                lock_file = f.read()
-
-            lock_bom = json.loads(lock_file)
-
-            if lock_bom.get('locked_by', None) == self.session_id:
-                os.remove(lock_path)
+        try:
+            print("Executing query")
+            self.cnx._lock()
+        except Exception as e:
+            print("Failed to execute query {}".format(e))
+        finally:
+            print("Unlocking db")
+            self.cnx._unlock()
